de963c55211a0296f4135945c46b145384c8f599
Scott M Sarsfield <scott.m.sarsfield@gmail.com>
2020-05-16 15:50:22 -0700

    fix up some mobile styles

--- packages/home/src/components/apps_page_content.js
+++ packages/home/src/components/apps_page_content.js
@@ -3,7 +3,7 @@ import orderBy from 'lodash/orderBy';
 import APPS from '../data/apps';
 import AppGallery from './app_gallery';
 
-const GamesPageContent = () => (
+const AppsPageContent = () => (
   <div className="row">
     {
       orderBy(APPS, ['archived'], ['asc']).map(({ archived, name, description, href, logoSrc, screenshots }, i) => (
@@ -20,4 +20,4 @@ const GamesPageContent = () => (
   </div>
 );
 
-export default GamesPageContent;
+export default AppsPageContent;

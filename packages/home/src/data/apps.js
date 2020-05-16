6850041941f0249dc19df794816120a600fc8073
Scott M Sarsfield <scott.m.sarsfield@gmail.com>
2020-05-16 13:31:18 -0700

    move pool pickers to /apps (cause it's not fun in and of itself)

--- packages/home/src/data/apps.js
+++ packages/home/src/data/apps.js
@@ -27,6 +27,26 @@ export default [
     ],
     href: '/apps/csf',
     imgSrc: 'http://scottmsarsfield.com/apps/csf/img/favicon.ico'
+  },
+
+  {
+    name: 'Pool Picker',
+    description: 'A utility for the billiards game Knockout. Select the number of players (3 or 5), then pass the device around until everyone knows their balls.',
+    tags: [
+      'Archived'
+    ],
+    href: '/apps/pool',
+    imgSrc: 'http://scottmsarsfield.com/games/pool/img/favicon.png'
+  },
+
+  {
+    name: 'Pool Picker 2',
+    description: 'A utility for the billiards game Knockout. Select the number of balls per player and tell your friends on other devices to join the group. Balls are assigned to each phone when group is ready.',
+    tags: [
+      'Archived'
+    ],
+    href: '/apps/pool2',
+    imgSrc: 'http://scottmsarsfield.com/games/pool2/img/favicon.png'
   }
 
 ];

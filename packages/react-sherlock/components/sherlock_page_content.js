6f3117382434c93360733a8af285b3c823623fc6
Scott M Sarsfield <scott.m.sarsfield@gmail.com>
2020-05-22 14:06:05 -0700

    dialogue text animation
    - althought I might scrap it

--- packages/react-sherlock/components/sherlock_page_content.js
+++ packages/react-sherlock/components/sherlock_page_content.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import types from 'prop-types';
 import { Helmet } from 'react-helmet';
+import isString from 'lodash/isString';
+import noop from 'lodash/noop';
 import background from './sherlock-background.jpg';
 import bigbear from './bigbear_sherlock.png';
 
@@ -54,6 +56,68 @@ const jrpgtextStyles = {
   letterSpacing: 2
 };
 
+const DialogueText = ({ children, onFinish = noop }) => {
+  const finished = useRef(false);
+  const [displayCount, setDisplayCount] = useState(1);
+  let childrenArray = React.Children.toArray(children);
+
+  if (childrenArray.length === 1 && isString(childrenArray[0])) {
+    childrenArray = childrenArray[0].split('');
+  }
+
+  const showNextChild = () => {
+    if (!finished.current && displayCount < childrenArray.length) {
+      setDisplayCount(displayCount + 1);
+    } else if (!finished.current) {
+      finished.current = true;
+      onFinish();
+    }
+  };
+
+  const handleFinish = () => {
+    showNextChild();
+  };
+
+  useEffect(() => {
+    if (childrenArray.length === 1) {
+      if (isString(childrenArray[0])) {
+        setTimeout(() => {
+          showNextChild();
+        }, 50);
+      } else {
+        showNextChild();
+      }
+    }
+  }, []);
+
+  if (childrenArray.length === 1) {
+    return children;
+  }
+
+  const currentChildren = childrenArray.slice(0, displayCount).map((elm, i) => (
+    <DialogueText key={i} onFinish={handleFinish}>
+      {elm}
+    </DialogueText>
+  ));
+
+  return currentChildren;
+};
+
+const Dialogue = ({ children, style, ...otherProps }) => {
+  return (
+    <div {...otherProps} style={{ ...style, position: 'relative' }}>
+      <div style={{ visibility: 'hidden' }}>
+        {children}
+      </div>
+      <div style={{ position: 'absolute', top: 10 }}>
+        <DialogueText>
+          {children}
+        </DialogueText>
+      </div>
+    </div>
+  );
+};
+
 const SherlockPageContent = () => (
   <div className="sherlock-page-content" style={{ margin: '0 20px' }}>
     <Helmet>
@@ -67,7 +131,7 @@ const SherlockPageContent = () => (
           <img src={bigbear} alt="Big Bear dressed as Sherlock" style={bigBearStyles} />
         </div>
         <div>
-          <div style={jrpgtextStyles}>
+          <Dialogue style={jrpgtextStyles}>
             {`
             Hi.  I want you to think of a number between 1 and 100. 
           `}
@@ -85,10 +149,10 @@ const SherlockPageContent = () => (
             <br />
             <br />
           Ready?
-          </div>
+          </Dialogue>
           <div style={{ ...jrpgtextStyles, display: 'flex', justifyContent: 'space-evenly' }}>
-            <button className="sherlock-button">Yes</button>
             <button className="sherlock-button">No</button>
+            <button className="sherlock-button">Yes</button>
           </div>
         </div>
       </div>

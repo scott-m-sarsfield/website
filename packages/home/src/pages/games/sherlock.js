3e03027b565db6f1d7bf070ccccb866008b0036b
Scott M Sarsfield <scott.m.sarsfield@gmail.com>
2020-05-24 18:19:52 -0700

    improve responsiveness (fill out available space where available)

--- packages/home/src/pages/games/sherlock.js
+++ packages/home/src/pages/games/sherlock.js
@@ -3,9 +3,12 @@ import React from 'react';
 import SherlockPageContent from '@scott-m-sarsfield/sherlock';
 import PageWithHeader from '../../components/shared/page_with_header';
 
+import './sherlock.scss';
+
 const SherlockPage = () => (
-  <PageWithHeader className="games-page" activeNav="games">
+  <PageWithHeader className="sherlock-page" activeNav="games">
     <div className="content">
+      <h3 className="title">Sherlock</h3>
       <SherlockPageContent />
     </div>
   </PageWithHeader>
--- packages/react-sherlock/components/sherlock_page_content.js
+++ packages/react-sherlock/components/sherlock_page_content.js
@@ -2,14 +2,11 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import SherlockGame from './sherlock_game';
 
-import './sherlock_page_content.scss';
-
 const SherlockPageContent = () => (
   <div className="sherlock-page-content">
     <Helmet>
       <link href="https://fonts.googleapis.com/css2?family=Ubuntu&family=Ubuntu+Mono&display=swap" rel="stylesheet" />
     </Helmet>
-    <h3 className="title">Sherlock</h3>
     <SherlockGame />
   </div>
 );

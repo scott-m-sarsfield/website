d16680656816edfe6cdd68326ec78c0a6a767034
Scott M Sarsfield <scott.m.sarsfield@gmail.com>
2020-05-24 21:15:17 -0700

    update games page to reflect remastered sherlock
    - ...and remove old sherlock games from list

--- packages/home/src/data/games.js
+++ packages/home/src/data/games.js
@@ -1,4 +1,23 @@
+import sherlockIcon from '../img/sherlock_icon.png';
+
 export default [
+  {
+    name: 'Sherlock',
+    tags: [
+      'Remastered'
+    ],
+    description: `
+      My original Sherlock game, dubbed 'Sherlock Holmes and the Random Number', was my first venture into
+      programming my own web-based games.  Inspired by a memory I had of a game I played at my aunt's that
+      could miraculously guess my card, I wanted to recapture and share that experience.  (Especially when
+      I learned about binary search in college.) I followed that up with 'Sherlock Holmes and the Mystery Card',
+      also written in PHP to fully realize the vision.  Some time later, I developed SherlockX, tackling the same
+      problem using JavaScript & jQuery, and now, to demonstrate some more modern experience, I present it once again
+      as simply 'Sherlock', written in React.
+    `,
+    href: '/games/sherlock',
+    imgSrc: sherlockIcon
+  },
   {
     name: 'Flood It',
     description: 'A simple game where you attempt to change the color the fewest number of times to fill the entire grid.',
@@ -8,7 +27,6 @@ export default [
     href: '/games/floodit',
     imgSrc: 'http://scottmsarsfield.com/games/floodit/img/favicon.png'
   },
-
   {
     name: 'Tetris',
     description: 'Blocks come crashing down and it\'s your job to guide them down so that complete [horizontal] lines can be formed (and magically vanish).',
@@ -19,7 +37,6 @@ export default [
     href: '/games/tetris',
     imgSrc: 'http://scottmsarsfield.com/games/tetris/img/favicon.png'
   },
-
   {
     name: 'Snake',
     description: 'A classic arcade game where the goal is to get to each colored red dot to grow the "snake" as long as you can before running into yourself.',
@@ -29,36 +46,5 @@ export default [
     ],
     href: '/games/snake',
     imgSrc: 'http://scottmsarsfield.com/games/snake/img/favicon.png'
-  },
-
-  {
-    name: 'Sherlock Holmes and the Random Number',
-    description: 'Sherlock Holmes (represented by my program) will deduce a number you choose from 1-100 asking you a series of Yes / No questions.',
-    tags: [
-      'Archived'
-    ],
-    href: '/games/sherlock',
-    imgSrc: 'http://scottmsarsfield.com/games/sherlock/img/favicon.png'
-  },
-
-  {
-    name: 'Sherlock Holmes and the Mystery Card',
-    description: 'Sherlock Holmes (represented by my program) will deduce a card you choose from a standard playing card deck by asking you a series of Yes / No questions.',
-    tags: [
-      'Archived'
-    ],
-    href: '/games/sherlock2',
-    imgSrc: 'http://scottmsarsfield.com/games/sherlock2/img/favicon.png'
-  },
-
-  {
-    name: 'Sherlock X',
-    description: 'Sherlock PHP games... in JavaScript.',
-    tags: [
-      'Archived'
-    ],
-    href: '/games/sherlockx',
-    imgSrc: 'http://scottmsarsfield.com/games/sherlockx/img/favicon.png'
   }
-
 ];

2b803eb76d572c04da193c5c3e6986c1d33eade4
Scott M Sarsfield <scott.m.sarsfield@gmail.com>
2020-05-25 14:40:03 -0700

    basic floodit gameplay

--- packages/react-floodit/components/floodit_game.js
+++ packages/react-floodit/components/floodit_game.js
@@ -1,39 +1,41 @@
 import React, { useReducer } from 'react';
-import fill from 'lodash/fill';
 import FloodItGrid from './floodit_grid';
 import FloodItButtons from './floodit_buttons';
-import { DIMENSION } from '../constants';
+import floodItReducer, { initialState, actions } from './floodit_reducer';
 
 import './floodit_game.scss';
-
-const actions = {
-  SELECT_COLOR: 'selectColor'
-};
-
-const initialState = {
-  grid: fill(new Array(DIMENSION * DIMENSION), 0)
-};
-
-const floodItGameplay = {
-  [actions.SELECT_COLOR]: (state, { color }) => ({
-    ...state,
-    grid: fill([...state.grid], color)
-  })
-};
-
-const floodItReducer = (state, { type, ...otherArgs }) => {
-  return floodItGameplay[type](state, { ...otherArgs });
-};
 
 const FloodItGame = () => {
   const [state, dispatch] = useReducer(floodItReducer, initialState);
   const {
-    grid
+    grid,
+    moveCount
   } = state;
   return (
     <div className="floodit-game">
-      <FloodItGrid grid={grid} />
-      <FloodItButtons onSelectColor={(color) => dispatch({ type: actions.SELECT_COLOR, color })} />
+      <div className="premise-menu">
+        <div>
+          <h5>Premise</h5>
+          <span>
+            {
+              `
+          Starting at the top left corner, click the color buttons below the grid to
+          change the color of the "flood" (the group of adjacent cells starting at the top-left).
+          `
+            }
+          </span>
+        </div>
+        <div className="restart-button">
+          <button onClick={() => dispatch({ type: actions.NEW_GAME })}>
+          New Game
+          </button>
+        </div>
+      </div>
+      <div className="game-area">
+        <div style={{ textAlign: 'right' }}>{moveCount}</div>
+        <FloodItGrid grid={grid} />
+        <FloodItButtons onSelectColor={(color) => dispatch({ type: actions.SELECT_COLOR, color })} />
+      </div>
     </div>
   );
 };

d1facaab88074e8c85316082c3c3f5c605f9f6c7
Scott M Sarsfield <scott.m.sarsfield@gmail.com>
2024-07-27 22:38:09 -0700

    WEB-0: add analysis section to kill the fun

--- packages/home/src/pages/games/tiktokcups.tsx
+++ packages/home/src/pages/games/tiktokcups.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import styled from 'styled-components';
 import random from 'lodash/random';
 
 import { Head as CommonHead } from '../../components/shared/PageWithHeader';
 import BannerMenuPage from '../../components/BannerMenuPage';
+import { orderBy } from 'lodash';
 
 export const Head = () => <CommonHead />;
 
@@ -154,6 +155,124 @@ const StyledGuessGrid = styled.div`
   }
 `;
 
+function getPermutations(str: string): string[] {
+  if (!str) {
+    return [''];
+  }
+  return str.split('').reduce((permutations, character, index) => {
+    return [
+      ...permutations,
+      ...getPermutations(`${str.slice(0, index)}${str.slice(index + 1)}`).map(
+        (p) => `${character}${p}`
+      ),
+    ];
+  }, [] as string[]);
+}
+
+const StyledSolutionSpace = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 200px);
+  flex-direction: column;
+  padding: 20px;
+  gap: 20px;
+`;
+
+const StyledAnalysisMiniCups = styled(StyledMiniCups)`
+  padding: 5px;
+  border-radius: 5px;
+  justify-content: center;
+  border: 1px solid transparent;
+
+  &:hover {
+    border-color: grey;
+  }
+
+  &[data-impossible] {
+    & > * {
+      opacity: 0.4;
+    }
+  }
+`;
+
+type Guesses = {
+  guess: string;
+  result: number;
+}[];
+
+// function isGuessPossible(guess: string, guesses: Guesses) {
+//   const possibleSolution = guess.split('');
+//   return guesses.every(({ guess, result }) => {
+//     return (
+//       possibleSolution.reduce((nCorrect, value, index) => {
+//         return nCorrect + (value === guess[index] ? 1 : 0);
+//       }, 0) === result
+//     );
+//   });
+// }
+
+function getGuessRejectionReason(
+  guess: string,
+  guesses: Guesses
+): string | null {
+  const possibleSolution = guess.split('');
+  for (let i = 0; i < guesses.length; i++) {
+    const g = guesses[i];
+    const expected = possibleSolution.reduce((nCorrect, value, index) => {
+      return nCorrect + (value === g.guess[index] ? 1 : 0);
+    }, 0);
+    if (expected !== g.result) {
+      return `result mismatch for guess ${i + 1}; expected ${expected}`;
+    }
+  }
+  return null;
+}
+
+const SolutionSpace = ({ guesses }: { guesses: Guesses }) => {
+  const allGuesses = useMemo(() => {
+    return getPermutations(INITIAL_GUESS);
+  }, []);
+
+  const analyses = useMemo(
+    () =>
+      orderBy(
+        allGuesses.map((guess) => {
+          const rejectionReason = getGuessRejectionReason(guess, guesses);
+          return {
+            guess,
+            possible: !rejectionReason,
+            reason: rejectionReason,
+          };
+        }),
+        ['possible'],
+        ['desc']
+      ),
+    [allGuesses, guesses]
+  );
+  const possibleCount = useMemo(
+    () => analyses.filter(({ possible }) => possible).length,
+    [analyses]
+  );
+  const allCount = useMemo(() => allGuesses.length, [allGuesses]);
+  return (
+    <>
+      {possibleCount} / {allCount}
+      <StyledSolutionSpace>
+        {analyses.map((guess, i) => (
+          <StyledAnalysisMiniCups
+            key={i}
+            data-impossible={!guess.possible || undefined}
+            title={guess.reason ?? ''}
+          >
+            {guess.guess.split('').map((l, i) => (
+              <StyledMiniCup key={i} data-code={l} />
+            ))}
+          </StyledAnalysisMiniCups>
+        ))}
+      </StyledSolutionSpace>
+    </>
+  );
+};
+
 const TikTokCupsContent = () => {
   const { restart, check } = useTikTokCupGame();
   const [guess, setGuess] = useState(INITIAL_GUESS);
@@ -207,6 +326,10 @@ const TikTokCupsContent = () => {
             </StyledGuessGrid>
           ))}
         </div>
+      </div>
+      <div>
+        <div>Analysis</div>
+        <SolutionSpace guesses={guesses} />
       </div>
     </StyledContent>
   );
